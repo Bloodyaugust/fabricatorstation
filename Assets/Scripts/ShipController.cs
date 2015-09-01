fe28f66d5506fc9bc9a90fcc28ecd3bd9fc5b42b
Bloodyaugust <greysonrichey@gmail.com>
2015-09-01 02:51:05 -0600

    Adding colliders to all ship components. Adding testcontroller for setting up perf test. Added stabilize functionality to ShipController, and initial randomization of torque to test. Added debug drawing for Attitude objects that shows their facing, will be used to calculate left/right facing for stabilize function.

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ShipController : MonoBehaviour {
 
+	List<GameObject> attitudeControllers;
+	Rigidbody2D physicsBody;
+
 	// Use this for initialization
 	void Start () {
-	
+		attitudeControllers = new List<GameObject>();
+		physicsBody = GetComponent<Rigidbody2D>();
+
+		physicsBody.AddTorque(10000);
+
+		foreach (Transform child in transform) {
+			if (child.gameObject.name == "Attitude") {
+				attitudeControllers.Add(child.gameObject);
+			}
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		foreach (GameObject attitude in attitudeControllers) {
+			Debug.DrawLine(attitude.transform.position, attitude.transform.position + attitude.transform.TransformDirection(attitude.transform.InverseTransformDirection(attitude.transform.up)) * -16, Color.red);
+		}
+	}
+
+	void FixedUpdate() {
+		Stabilize();
+	}
+
+	void Stabilize() {
+		if (physicsBody.angularVelocity > 0) {
+			physicsBody.AddTorque(-10.5f);
+		} else if (physicsBody.angularVelocity < 0) {
+			physicsBody.AddTorque(10.5f);
+		}
+
+		if (Mathf.Abs(physicsBody.angularVelocity) < 1f) {
+			physicsBody.angularVelocity = 0;
+		}
 	}
 }
---
+++ Assets/Scripts/TestController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestController : MonoBehaviour {
+
+	public GameObject shipPrefab;
+
+	// Use this for initialization
+	void Start () {
+		float screenX;
+		float screenY;
+		Vector3 newPoint;
+
+		for (int i = 0; i < 0; i++) {
+			screenX = Random.Range(0.0f, Camera.main.pixelWidth);
+			screenY = Random.Range(0.0f, Camera.main.pixelHeight);
+			newPoint = Camera.main.ScreenToWorldPoint(new Vector3(screenX, screenY, 10));
+
+			Instantiate(shipPrefab, newPoint, Quaternion.identity);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
